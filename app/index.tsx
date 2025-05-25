1c0c40ab467c1f6d945ed05b1ef1b26e4a386169
matveevanastyaa <matvnastya@gmail.com>
2025-05-25 14:25:40 +0200

    home page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,50 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Coffee Shop!</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello Anastasia</Text>
+      <Text style={styles.subtitle}>Bienvenue à Academy of Coffee !</Text>
+      <TouchableOpacity style={styles.buttonContainer} activeOpacity={0.8}>
+        <Text style={styles.buttonText}>Go to coffeeshop</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 30,
+    color: "brown",
+    backgroundColor: "beige",
+    fontWeight: 'bold',
+    borderWidth: 2,
+    borderColor: 'brown',
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 20,
+    backgroundColor: 'beige',
+    fontStyle: 'italic',
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    width: 245,
+    height: 50,
+    borderRadius: 20,
+    backgroundColor: '#DDB892',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'white',
+    alignSelf: 'center',
+  }
+});
